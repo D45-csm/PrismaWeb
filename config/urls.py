8d43142be9ca71413219cac58d17787ba725160b
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-09-20 00:08:52 -0500

    url de la app insumos agregada a las configuraciones

--- config/urls.py
+++ config/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('comprobantes/', include('APP.comprobantes.urls')),
     path('compra_insumos/', include('APP.compra_insumos.urls')),
     path('carrito/', include('APP.carrito.urls')),
+    path('insumos/', include('APP.insumos.urls')),
 ]
