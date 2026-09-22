1c8e1e8d4b59926bab57bf993504e283fcae0cfc
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-09-22 15:23:33 -0500

    modelos agregados: CategoriaProducto y Producto

--- APP/productos/models.py
+++ APP/productos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class CategoriaProducto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(CategoriaProducto, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='productos/')
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
