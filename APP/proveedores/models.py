bbffee0b6169ed3586acfada373a83e472192d65
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-09-22 15:24:01 -0500

    modelos agregados: Proveedor

--- APP/proveedores/models.py
+++ APP/proveedores/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.TextField(blank=True, null=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
